183b5459bd39d14585a760852a468ca398cac138
Moriarty123 <1561357687@qq.com>
2018-12-06 11:38:41 +0800

    20018-12-6 demo2 ave
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- demo2.php
+++ demo2.php
@@ -21,7 +21,7 @@
 // // }
 
 // for($i = 0; $i < count($array); $i++) {
-	
+
 // 	$row = $array[$i];
 // 	$row['sex'] = 'man';
 // 	$array[$i] = $row;
@@ -36,7 +36,7 @@
 // 	$a++;
 
 // 	if ($a < 5) {
-		
+
 // 		test();
 // 	}
 // 	echo $a;
@@ -51,8 +51,11 @@ $data2 = [1,2,3];
 
 $data = [
 	['A','B','C'],
-	['1','2','3']
+	['1','2','3'],
+	['a','b']
 ];
+
+//
 
 function deal($data) {
 
@@ -70,8 +73,72 @@ function deal($data) {
 	return $res;
 }
 
-$res = deal($data);
+function sum($data) {
 
-foreach ($res as $key => $value) {
-	echo $value.'<br />';
+	$res = [];
+	$count = count($data);
+
+	if ($count > 1) {
+		for($i = 0; $i < count($data[0]); $i++) {
+			for($j = 0; $j < count($data[1]); $j++) {
+				$res[] = $data[0][$i].$data[1][$j];
+			}
+
+		}
+		
+		$data[0] = $res;
+		array_splice($data, 1,1);
+		return sum($data);
+	}
+	else {
+		return $data[0];
+	}
+
 }
+
+
+// $res = sum($data);
+
+echo "<pre>";
+// var_dump($res);
+
+
+function ave($data) {
+
+	$strlen = count($data);
+	// print_r($data[0]);
+	if($strlen > 1) {
+		$result = [];
+
+		for($i = 0; $i < count($data[0]); $i++) {
+			for($j = 0; $j < count($data[1]); $j++) {
+				// print_r($data[0][$i]);
+				if(is_array($data[0][$i])) {
+					$temp = $data[0][$i];
+					$temp[] = $data[1][$j];
+				}
+				else {
+					$temp = array($data[0][$i],$data[1][$j]);
+				}
+				$result[] = $temp;
+				// print_r($result);
+			}
+		}
+		
+		$data[0] = $result;
+		array_splice($data,1,1);
+		return ave($data);
+
+	}
+	else {
+		return $data;
+	}
+
+}
+$res = ave($data);
+echo '<pre>';
+// print_r($res);
+
+
+// print_r($res);
+var_dump($res);
